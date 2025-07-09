77678e17b9c5a71c1ab5abaf2588702a3d44346a
shakshiM <196899476+Mshakshi-777@users.noreply.github.com>
2025-07-09 12:58:28 +0530

    Added 15_reverse_array assignment

---
+++ numpy_basics/numpy_basics/broadcasting_operations/array_manipulation/array_manipulation/array_manipulation/array_manipulation/array_manipulation/indexing_slicing/indexing_slicing/indexing_slicing/indexing_slicing/indexing_slicing/indexing_slicing/15_reverse_array.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Create a 1D array with 10 elements
+array = np.arange(1, 11)
+
+print("Original Array:")
+print(array)
+
+# Reverse the array using slicing
+reversed_array = array[::-1]
+
+print("\nReversed Array:")
+print(reversed_array)
